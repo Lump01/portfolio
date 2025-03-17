6f851f9bedfac0d9a96431b61aca86cb7d577c99
jlump-jbh <jlump.jukeboxhero@gmail.com>
2025-03-16 20:14:59 -0500

    various qol updates

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'jonathan.a.lumpkin@gmail.com',
+    phone: '+2147840250',
+    address: 'Abilene, TX, USA',
+    github: 'https://github.com/lump01',
+    facebook: 'https://www.facebook.com/jonathan.lumpkin/',
+    linkedIn: 'https://www.linkedin.com/in/jonathan.lumpkin/',
+    twitter: 'https://twitter.com/jonathanlumpkin',
+    stackOverflow: "https://stackoverflow.com/users/20284548/lump",
+    devUsername: "lump001"
 }
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,25 +1,24 @@
 export const personalData = {
-  name: 'Jonathan Lumpkin',
-  profile: '/profile.png',
-  designation: 'Software Developer',
+  name: "Jonathan Lumpkin",
+  profile: "/profile.png",
+  designation: "Software Developer",
   personalDescription:
-      "Hey there! My name is Jonathan Lumpkin and I've been married to my beautiful wife, Gretchen, since November of 2021, and just had my firstborn, Josephine. ",
+    "Hey there! My name is Jonathan Lumpkin and I've been married to my beautiful wife, Gretchen, since November of 2021, and we just had our firstborn, Josephine. ",
   devDescription:
-      "I'm an innovative software developer with four years of experience with a strong " +
-      'background in mobile and web application development and full-stack development. I have a proven ability ' +
-      'to lead feature development for award-winning applications, optimize workflows with automation, ' +
-      "and enhance user experiences through intuitive design. I'm passionate about building impactful " +
-      'technology, driving projects forward, and continually expanding expertise in software engineering and leadership.',
-  email: 'jonathan.a.lumpkin@gmail.com',
-  phone: '+1 (214) 784-0250',
-  address: 'Abilene, TX, USA',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: 'https://leetcode.com/said3812/',
-  devUsername: 'said7388',
+    "I'm an innovative software developer with four years of experience and a strong background in full-stack mobile" +
+    "and web application development. I have a proven ability to lead feature development for award-winning " +
+    "applications, optimize workflows with automation, and enhance user experiences through intuitive design. I'm " +
+    "passionate about building impactful technology, driving projects forward, and continually expanding expertise in " +
+    "software engineering and leadership.",
+  email: "jonathan.a.lumpkin@gmail.com",
+  address: "Abilene, TX, USA",
+  github: "https://github.com/lump01",
+  facebook: "https://www.facebook.com/jonathanlumpkin/",
+  linkedIn: "https://www.linkedin.com/in/jonathan-lumpkin-1060391a6/",
+  twitter: "https://twitter.com/jonathanlumpkin",
+  stackOverflow: "https://stackoverflow.com/users/20284548/lump",
+  leetcode: "https://leetcode.com/u/lump001/",
+  devUsername: "lump001",
   resume:
-      'https://drive.google.com/file/d/1mkjMlwlkS15izGba5AVyY3SEh4C5cBZ6/view?usp=sharing',
+    "https://drive.google.com/file/d/1mkjMlwlkS15izGba5AVyY3SEh4C5cBZ6/view?usp=sharing",
 };
